1f40398b93d408d1a566fbddfcf146faf5932e5c
facturacionObrasSociales <153233253+facturacionObrasSociales@users.noreply.github.com>
2023-12-19 20:52:03 -0300

    Unifica formato de tablas
    Ahora si hay contenido en la celda B1 del archivo de excel, se creará una tabla donde el contenido de la fila 1 funcionará como encabezado de cada columna.

--- main.js
+++ main.js
@@ -27,7 +27,6 @@ function interpretarExcel(archivo){
         workbook.SheetNames.forEach(function(sheetName, index) {
             var worksheet = workbook.Sheets[sheetName];
             var jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-            
             const filaTitulo = document.createElement('tr');
             const encabezado = document.createElement('th');
             encabezado.textContent = sheetName;
@@ -50,66 +49,12 @@ function interpretarExcel(archivo){
 
             encabezado.appendChild(botonMostrarOcultar);
             filaTitulo.appendChild(encabezado);
-            
+
             const filaContenido = document.createElement('tr');
             filaContenido.setAttribute('id', `contenido-${index + 1}`);
             const celdaContenido = document.createElement('td');
 
-            if(sheetName !== 'Codigos' && sheetName !== 'Facturacion'){  
-                celdaContenido.colSpan = 2;                  
-                for(let i = 0; i < jsonData.length; i++){
-                    if(jsonData[i][0]){
-                        const contenidoParrafo = document.createElement('p');
-                        contenidoParrafo.textContent = jsonData[i][0];
-                        if (i === 0 && jsonData[i].length === 1) {
-                            // Si es la celda A1, mostrar el contenido en h3 con estilo especial
-                            const contenidoH3 = document.createElement('h3');
-                            contenidoH3.textContent = jsonData[i][0];
-                            contenidoH3.classList.add('contenidoH3');
-                            celdaContenido.appendChild(contenidoH3);
-                        } else if (jsonData[i][0].endsWith(':')) {
-                            const contenidoH3 = document.createElement('h3');
-                            contenidoH3.textContent = jsonData[i][0];
-                            celdaContenido.appendChild(contenidoH3);
-                        } else if (jsonData[i][0].endsWith('cluye:')) {
-                            const contenidoH4 = document.createElement('h4');
-                            contenidoH4.textContent = jsonData[i][0];
-                            celdaContenido.appendChild(contenidoH4);
-                        } else {
-                            celdaContenido.appendChild(contenidoParrafo);
-                        }
-                    }   
-                }
-            }
-
-            else if(sheetName === 'Codigos'){
-                if(jsonData[0][1] == 'PRACTICA OSDE'){
-                    const contenidoH3 = document.createElement('h3');
-                    contenidoH3.textContent = 'PRACTICA OSDE';
-                    contenidoH3.classList.add('titulo-codigo');
-                    celdaContenido.appendChild(contenidoH3);
-                }
-                for (let i = 1; i < jsonData.length; i++) {
-                    const filaCodigos = document.createElement('tr');
-        
-                    const celdaColumnaA = document.createElement('td');
-                    celdaColumnaA.textContent = jsonData[i][0] || '';
-        
-                    const celdaColumnaB = document.createElement('td');
-                    celdaColumnaB.textContent = jsonData[i][1] || '';
-        
-                    const celdaColumnaC = document.createElement('td');
-                    celdaColumnaC.textContent = jsonData[i][2] || '';
-
-                    filaCodigos.appendChild(celdaColumnaA);
-                    filaCodigos.appendChild(celdaColumnaB);
-                    filaCodigos.appendChild(celdaColumnaC);
-                    
-                    celdaContenido.appendChild(filaCodigos);
-                    tabla.appendChild(celdaContenido);
-                }
-            }
-            else if(sheetName === 'Facturacion'){
+            if(sheetName === 'Facturacion' || sheetName === 'Facturación'){
                 for(let i = 0; i < jsonData.length; i++){
                     if(jsonData[i][0]){
                         const contenidoParrafo = document.createElement('p');
@@ -124,6 +69,61 @@ function interpretarExcel(archivo){
                     }   
                 }
             }
+            else if(!ContenidoB1(worksheet)){  
+                celdaContenido.colSpan = 2;                  
+                for(let i = 0; i < jsonData.length; i++){
+                    if(jsonData[i][0]){
+                        const contenidoParrafo = document.createElement('p');
+                        contenidoParrafo.textContent = jsonData[i][0];
+                        if (i === 0 && jsonData[i].length === 1) {
+                            // Si es la celda A1, mostrar el contenido en h3 con estilo especial
+                            const contenidoH3 = document.createElement('h3');
+                            contenidoH3.textContent = jsonData[i][0];
+                            contenidoH3.classList.add('contenidoH3');
+                            celdaContenido.appendChild(contenidoH3);
+                        } else if (typeof jsonData[i][0] === 'string' &&jsonData[i][0].endsWith(':')) {
+                            const contenidoH3 = document.createElement('h3');
+                            contenidoH3.textContent = jsonData[i][0];
+                            celdaContenido.appendChild(contenidoH3);
+                        } else if (typeof jsonData[i][0] === 'string' && jsonData[i][0].endsWith('cluye:')) {
+                            const contenidoH4 = document.createElement('h4');
+                            contenidoH4.textContent = jsonData[i][0];
+                            celdaContenido.appendChild(contenidoH4);
+                        } else {
+                            celdaContenido.appendChild(contenidoParrafo);
+                        }
+                    }   
+                }
+            }
+            else {
+                for (let i = 0; i < jsonData.length; i++) {
+                    const filaCodigos = document.createElement('tr');
+        
+                    const celdaColumnaA = document.createElement('td');
+                    celdaColumnaA.textContent = jsonData[i][0] || '';
+        
+                    const celdaColumnaB = document.createElement('td');
+                    celdaColumnaB.textContent = jsonData[i][1] || '';
+        
+                    const celdaColumnaC = document.createElement('td');
+                    celdaColumnaC.textContent = jsonData[i][2] || '';
+
+                    if(i == 0){
+                        celdaColumnaA.style.fontWeight = 'bold';
+                        celdaColumnaB.style.fontWeight = 'bold';
+                        celdaColumnaB.style.textAlign = 'center';
+                    }
+
+                    if(!filaVacia(celdaColumnaA, celdaColumnaB,celdaColumnaC)){
+                        filaCodigos.appendChild(celdaColumnaA);
+                        filaCodigos.appendChild(celdaColumnaB);
+                        filaCodigos.appendChild(celdaColumnaC);
+                        celdaContenido.appendChild(filaCodigos);
+                    }
+                    
+                    tabla.appendChild(celdaContenido);
+                }
+            }
 
             filaContenido.appendChild(celdaContenido);
             filaContenido.style.display = 'none';
@@ -132,4 +132,13 @@ function interpretarExcel(archivo){
         });
     };
     reader.readAsArrayBuffer(archivo);
+}
+
+function ContenidoB1(worksheet){
+    var cellValue = worksheet['B1'] ? worksheet['B1'].v : '';
+    return cellValue.trim();
+}
+
+function filaVacia(celdaA, celdaB, celdaC){
+    return celdaA.textContent === '' && celdaB.textContent === '' && celdaC.textContent === '';
 }
